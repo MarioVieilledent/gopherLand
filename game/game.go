9a4764452112d38da2f0f43fa3b57207b5592609
Mario Vieilledent <mario.vieilledent@gmail.com>
2022-09-12 22:47:41 +0200

    Player gravity, jump, dynamic sized map, refact.

--- game/game.go
+++ game/game.go
@@ -6,9 +6,6 @@ import (
 	"strings"
 )
 
-var MapSizeWidth int = 21
-var MapSizeHeight int = 21
-
 type Elem struct {
 	Name   string          // Name of element
 	Short  rune            // Short identifier for elems (to build maps)
@@ -23,17 +20,25 @@ type ImagePosition struct {
 	Y2 int
 }
 
+type Position struct {
+	X float64
+	Y float64
+}
+
 type Player struct {
-	X         float64
-	Y         float64
-	Speed     float64
-	Direction rune // l or r
+	Position         Position
+	EatBox           [4]float64 // Distance from center (position) (up, right, down, right)
+	Speed            float64
+	Direction        rune    // l or r
+	TouchingGround   bool    // True if player is walking, false if falling or jumping
+	VerticalVelocity float64 // Vertical velocity on air (gravity falling, or gravity jumping)
+	Walking          bool    // Animate player when walking
 }
 
 type Game struct {
 	Ss          int           // Size of square elements
 	width       int           // Number of blocks (width)
-	height      int           // Number of blocks (heigth)
+	height      int           // Number of blocks (height)
 	AllElements map[rune]Elem // All elements
 	GameMap     [][]rune      // Game map
 	Player      Player
@@ -43,41 +48,117 @@ func InitGame() Game {
 	// Init game structure
 	game := Game{
 		64,
-		MapSizeWidth,
-		MapSizeHeight,
+		0,
+		0,
 		map[rune]Elem{},
 		[][]rune{},
-		Player{0.0, 0.0, 0.08, 'r'},
+		Player{
+			Position{1.5, 1.5},
+			[4]float64{0.45, 0.4, 0.5, 0.4},
+			0.08,
+			'r',
+			false,
+			0.0,
+			false,
+		},
 	}
-	game.loadRessources()
+	game.loadResources()
 	game.createMap()
 	return game
 }
 
-func (game *Game) createMap() {
-	for w := 0; w < game.width; w++ {
-		game.GameMap = append(game.GameMap, []rune{})
-		for h := 0; h < game.height; h++ {
-			game.GameMap[w] = append(game.GameMap[w], ' ')
+///////////////////
+// GAME METHODS ///
+///////////////////
+
+// Checks if player is touching the ground (not falling or jumping)
+func (g *Game) CheckIfTouchesGround() bool {
+	x := int(g.Player.Position.X)
+	y := int(g.Player.Position.Y + g.Player.EatBox[2])
+	if y < g.height-1 {
+		if g.AllElements[g.GameMap[x][y]].Solid {
+			g.Player.TouchingGround = true
+			return true
+		} else {
+			g.Player.TouchingGround = false
 		}
 	}
+	return false
+}
 
+// Check if player touches a block while jumping
+func (g *Game) CheckIfTouchesCeil() bool {
+	x := int(g.Player.Position.X)
+	y := int(g.Player.Position.Y - g.Player.EatBox[0])
+	if y < 0 {
+		return true
+	}
+	return g.AllElements[g.GameMap[x][y]].Solid
+}
+
+func (game *Game) createMap() {
+	// Reads the resources file
 	file, err := ioutil.ReadFile("data/map.txt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // /!\ Need a better handle of the error here /!\
 	} else {
+		// Takes all lines as a slice of strings
 		lines := strings.Split(string(file), "\n")
+
+		// Get the size of the map
+		game.height = len(lines)
+		game.width = longestStr(lines)
+
+		// Generate empty map
+		for w := 0; w < game.width; w++ {
+			game.GameMap = append(game.GameMap, []rune{})
+			for h := 0; h < game.height; h++ {
+				game.GameMap[w] = append(game.GameMap[w], ' ')
+			}
+		}
+
+		// Fill the map
 		for x, l := range lines {
 			for y, c := range l {
 				if c != ' ' {
-					game.GameMap[x][y] = c
+					game.GameMap[y][x] = c
 				}
 			}
 		}
 	}
 }
 
-func (game *Game) loadRessources() {
+// While moving toward left or right, checks for obstacle on the way
+func (g Game) CheckEmptyBlock(direction string) bool {
+	upMostPoint := int(g.Player.Position.Y - g.Player.EatBox[0])
+	rightMostPoint := int(g.Player.Position.X + g.Player.EatBox[1])
+	downMostPoint := int(g.Player.Position.Y + g.Player.EatBox[2])
+	leftMostPoint := int(g.Player.Position.X - g.Player.EatBox[3])
+	x := int(g.Player.Position.X)
+	y := int(g.Player.Position.Y)
+	switch direction {
+	case "up":
+		if upMostPoint >= 0 {
+			return !g.AllElements[g.GameMap[x][upMostPoint]].Solid
+		}
+	case "right":
+		if rightMostPoint < g.width-1 {
+			return !g.AllElements[g.GameMap[rightMostPoint][y]].Solid
+		}
+	case "down":
+		if downMostPoint < g.height-1 {
+			return !g.AllElements[g.GameMap[x][downMostPoint]].Solid
+		}
+	case "left":
+		if leftMostPoint >= 0 {
+			return !g.AllElements[g.GameMap[leftMostPoint][y]].Solid
+		}
+	}
+	return false
+}
+
+// Loads all resources as element
+func (game *Game) loadResources() {
 	game.loadRessource("stone", 's', true, []ImagePosition{{0, 1, 0, 1}})
 	game.loadRessource("dirt", 'd', true, []ImagePosition{{1, 2, 0, 1}})
 	game.loadRessource("grass", 'g', true, []ImagePosition{{2, 3, 0, 1}})
@@ -87,9 +168,10 @@ func (game *Game) loadRessources() {
 	game.loadRessource("coin_1", 'c', false, []ImagePosition{{0, 1, 2, 3}, {1, 2, 2, 3},
 		{2, 3, 2, 3}, {3, 4, 2, 3}, {4, 5, 2, 3}, {5, 6, 2, 3}})
 	game.loadRessource("player", 'p', false, []ImagePosition{{0, 1, 3, 4}, {1, 2, 3, 4},
-		{2, 3, 3, 4}, {3, 4, 3, 4}, {4, 5, 3, 4}, {5, 6, 3, 4}, {5, 6, 3, 4}, {7, 8, 3, 4}})
+		{2, 3, 3, 4}, {3, 4, 3, 4}, {4, 5, 3, 4}, {5, 6, 3, 4}, {6, 7, 3, 4}, {7, 8, 3, 4}})
 }
 
+// Loads a single element
 func (game *Game) loadRessource(name string, short rune, solid bool, images []ImagePosition) {
 	ip := []ImagePosition{}
 
@@ -108,4 +190,18 @@ func (game *Game) loadRessource(name string, short rune, solid bool, images []Im
 		solid,
 		ip,
 	}
+}
+
+/////////////////////
+// OTHER FUNCTIONS //
+/////////////////////
+
+// Find the longest string of an array (used for get the width of the map depending on)
+func longestStr(arr []string) (max int) {
+	for _, v := range arr {
+		if len(v) > max {
+			max = len(v)
+		}
+	}
+	return
 }
--- graphic/window.go
+++ graphic/window.go
@@ -17,16 +17,23 @@ type Controller struct {
 }
 
 var backgroundImage *ebiten.Image
-var ressourcesImage *ebiten.Image
+var resourcesImage *ebiten.Image
 
-func initControler() Controller {
+var playerShift float64 // Shift for displaying player
+
+//////////////////////////////
+// INITIALIZATION FUNCTIONS //
+//////////////////////////////
+
+func initController() Controller {
 	g := game.InitGame()
+	playerShift = 0.5 * float64(g.Ss)
 	return Controller{&g, 0, 0}
 }
 
 func init() {
 	var err error
-	ressourcesImage, _, err = ebitenutil.NewImageFromFile("data/ressources.png")
+	resourcesImage, _, err = ebitenutil.NewImageFromFile("data/ressources.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,6 +43,178 @@ func init() {
 	}
 }
 
+//////////////////////
+// UPDATE FUNCTIONS //
+//////////////////////
+
+// Update function, called each frame
+func (c *Controller) Update() error {
+	// Tick management (each 12 frames = 200 ms)
+	c.manageTick()
+
+	previousIsTouchingGround := c.game.Player.TouchingGround
+	isTouchingGround := c.game.CheckIfTouchesGround()
+
+	// If player touches the ground
+	c.manageTouchingGround(previousIsTouchingGround, isTouchingGround)
+
+	// Manages jumping
+	c.manageJumpOrFall(isTouchingGround)
+
+	// Manages button clicks
+	c.manageButtonClicks(isTouchingGround)
+
+	return nil
+}
+
+// Manages tick for modulo for animate elements
+func (c *Controller) manageTick() {
+	if c.tickFrame == 6 {
+		c.tickFrame = 0
+		c.tick++
+	} else {
+		c.tickFrame++
+	}
+}
+
+// Manages if the player landed
+func (c *Controller) manageTouchingGround(previousIsTouchingGround, isTouchingGround bool) {
+	if !previousIsTouchingGround && isTouchingGround {
+		c.game.Player.VerticalVelocity = 0.0 // Reset the velocity of player
+		// Avoid player to be under the ground (falling too deep with decrementations)
+		c.game.Player.Position.Y = math.Floor(c.game.Player.Position.Y) + 0.50001
+	}
+}
+
+// Manages jump and fall of player
+func (c *Controller) manageJumpOrFall(isTouchingGround bool) {
+	if !isTouchingGround {
+		// Forces the player to stop walking while on the air
+		if c.game.Player.Walking {
+			c.game.Player.Walking = false
+		}
+		// Move verticaly the player sdepending on its vertical velocity
+		c.game.Player.Position.Y += (0.01 * c.game.Player.VerticalVelocity)
+		// If player hit ceil while jumping
+		if c.game.CheckIfTouchesCeil() {
+			c.game.Player.VerticalVelocity = 0 // Reset of its velocity
+			// Forces player to not be inside block
+			c.game.Player.Position.Y = math.Floor(c.game.Player.Position.Y) + 0.49999
+		} else {
+			c.game.Player.VerticalVelocity += 1.0
+		}
+	} else if c.game.Player.VerticalVelocity != 0 {
+		c.game.Player.VerticalVelocity = 0
+	}
+}
+
+// Manages input for controlling player
+func (c *Controller) manageButtonClicks(isTouchingGround bool) {
+	var notClickedLeft, notClickedRight bool
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		c.game.Player.Direction = 'l'
+		if c.game.CheckEmptyBlock("left") {
+			if !c.game.Player.Walking && isTouchingGround {
+				c.game.Player.Walking = true
+			}
+			c.game.Player.Position.X -= c.game.Player.Speed
+		}
+	} else {
+		notClickedLeft = true
+	}
+
+	// Right button clicked
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		c.game.Player.Direction = 'r'
+		if c.game.CheckEmptyBlock("right") {
+			if !c.game.Player.Walking && isTouchingGround {
+				c.game.Player.Walking = true
+			}
+			c.game.Player.Position.X += c.game.Player.Speed
+		}
+	} else {
+		notClickedRight = true
+	}
+
+	if notClickedLeft && notClickedRight && c.game.Player.Walking {
+		c.game.Player.Walking = false
+	}
+
+	// Up button clicked
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		if isTouchingGround {
+			c.game.Player.TouchingGround = false
+			c.game.Player.VerticalVelocity = -20.0
+			c.game.Player.Position.Y -= 0.01
+		}
+	}
+}
+
+///////////////////////
+// DRAWING ON WINDOW //
+///////////////////////
+
+func (c *Controller) Draw(screen *ebiten.Image) {
+	screen.DrawImage(backgroundImage, nil)
+	c.displayBlocks(screen)
+	c.displayPlayer(screen)
+}
+
+// Draw all blocks of the map
+func (c *Controller) displayBlocks(screen *ebiten.Image) {
+	for x := 0; x < len(c.game.GameMap); x++ {
+		for y := 0; y < len(c.game.GameMap[x]); y++ {
+
+			elem := c.game.AllElements[c.game.GameMap[x][y]]
+
+			modulo := c.getModulo(elem)
+
+			if len(elem.Images) > 0 {
+				op := &ebiten.DrawImageOptions{}
+				op.GeoM.Translate(float64(c.game.Ss*x), float64(c.game.Ss*y))
+				screen.DrawImage(resourcesImage.SubImage(
+					image.Rect(elem.Images[modulo].X1, elem.Images[modulo].Y1,
+						elem.Images[modulo].X2, elem.Images[modulo].Y2)).(*ebiten.Image), op)
+			}
+		}
+	}
+}
+
+// Draw the player
+func (c *Controller) displayPlayer(screen *ebiten.Image) {
+	modulo := c.getModulo(c.game.AllElements['p'])
+
+	op := &ebiten.DrawImageOptions{}
+	if c.game.Player.Direction == 'l' {
+		// Mirroring image
+		op.GeoM.Scale(-1, 1)
+		op.GeoM.Translate(float64(c.game.Ss)-playerShift, -playerShift)
+	} else {
+		op.GeoM.Translate(-playerShift, -playerShift)
+	}
+	op.GeoM.Translate(c.game.Player.Position.X*float64(c.game.Ss),
+		c.game.Player.Position.Y*float64(c.game.Ss))
+	if c.game.Player.Walking {
+		screen.DrawImage(resourcesImage.SubImage(
+			image.Rect(c.game.AllElements['p'].Images[modulo].X1,
+				c.game.AllElements['p'].Images[modulo].Y1,
+				c.game.AllElements['p'].Images[modulo].X2,
+				c.game.AllElements['p'].Images[modulo].Y2)).(*ebiten.Image), op)
+	} else {
+		screen.DrawImage(resourcesImage.SubImage(
+			image.Rect(c.game.AllElements['p'].Images[1].X1,
+				c.game.AllElements['p'].Images[1].Y1,
+				c.game.AllElements['p'].Images[1].X2,
+				c.game.AllElements['p'].Images[1].Y2)).(*ebiten.Image), op)
+	}
+}
+
+/////////////////////
+// OTHER FUNCTIONS //
+/////////////////////
+
+// Used to make animation of elements
 func (c Controller) getModulo(elem game.Elem) int {
 	max := len(elem.Images)
 	modulo := 0
@@ -45,105 +224,6 @@ func (c Controller) getModulo(elem game.Elem) int {
 	return modulo
 }
 
-func (c Controller) checkEmptyBlock(direction string) bool {
-	switch direction {
-	case "left":
-		x := int(c.game.Player.X)
-		y := int(math.Ceil(c.game.Player.Y))
-		if y > 0 {
-			return !c.game.AllElements[c.game.GameMap[x][y-1]].Solid
-		}
-	case "right":
-		x := int(c.game.Player.X)
-		y := int(c.game.Player.Y)
-		if y < game.MapSizeWidth {
-			return !c.game.AllElements[c.game.GameMap[x][y+1]].Solid
-		}
-	case "up":
-		x := int(math.Ceil(c.game.Player.X))
-		y := int(c.game.Player.Y)
-		if x > 0 {
-			return !c.game.AllElements[c.game.GameMap[x-1][y]].Solid
-		}
-	case "down":
-		x := int(c.game.Player.X)
-		y := int(c.game.Player.Y)
-		if x < game.MapSizeWidth {
-			return !c.game.AllElements[c.game.GameMap[x+1][y]].Solid
-		}
-	}
-	return false
-}
-
-func (c *Controller) Update() error {
-	// Tick management (each 12 frames = 200 ms)
-	if c.tickFrame == 12 {
-		c.tickFrame = 0
-		c.tick++
-	} else {
-		c.tickFrame++
-	}
-
-	// Control management
-	// x, y := ebiten.CursorPosition()
-	// fmt.Println(x, y)
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		c.game.Player.Direction = 'l'
-		if c.checkEmptyBlock("left") {
-			c.game.Player.Y -= c.game.Player.Speed
-		}
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		c.game.Player.Direction = 'r'
-		if c.checkEmptyBlock("right") {
-			c.game.Player.Y += c.game.Player.Speed
-		}
-	}
-
-	// Player gravity
-	if c.checkEmptyBlock("down") {
-		c.game.Player.X += 0.1
-	}
-
-	return nil
-}
-
-func (c *Controller) Draw(screen *ebiten.Image) {
-	screen.DrawImage(backgroundImage, nil)
-
-	// For each block (Elem)
-	for y := 0; y < len(c.game.GameMap); y++ {
-		for x := 0; x < len(c.game.GameMap[y]); x++ {
-
-			elem := c.game.AllElements[c.game.GameMap[x][y]]
-
-			modulo := c.getModulo(elem)
-
-			if len(elem.Images) > 0 {
-				op := &ebiten.DrawImageOptions{}
-				op.GeoM.Translate(float64(c.game.Ss*y), float64(c.game.Ss*x))
-				screen.DrawImage(ressourcesImage.SubImage(
-					image.Rect(elem.Images[modulo].X1, elem.Images[modulo].Y1,
-						elem.Images[modulo].X2, elem.Images[modulo].Y2)).(*ebiten.Image), op)
-			}
-		}
-	}
-
-	// Player display
-	modulo := c.getModulo(c.game.AllElements['p'])
-
-	op := &ebiten.DrawImageOptions{}
-	if c.game.Player.Direction == 'l' {
-		// Mirroring image
-		op.GeoM.Scale(-1, 1)
-		op.GeoM.Translate(float64(c.game.Ss), 0)
-	}
-	op.GeoM.Translate(c.game.Player.Y*float64(c.game.Ss), c.game.Player.X*float64(c.game.Ss))
-	screen.DrawImage(ressourcesImage.SubImage(
-		image.Rect(c.game.AllElements['p'].Images[modulo].X1, c.game.AllElements['p'].Images[modulo].Y1,
-			c.game.AllElements['p'].Images[modulo].X2, c.game.AllElements['p'].Images[modulo].Y2)).(*ebiten.Image), op)
-}
-
 func (c *Controller) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 1280, 720
 }
@@ -151,7 +231,7 @@ func (c *Controller) Layout(outsideWidth, outsideHeight int) (screenWidth, scree
 func OpenWindow() {
 	ebiten.SetWindowSize(1280, 720)
 
-	controler := initControler()
+	controler := initController()
 
 	err := ebiten.RunGame(&controler)
 
